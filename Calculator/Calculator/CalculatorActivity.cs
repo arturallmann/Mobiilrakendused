a7318ffd929b09a120b3927cb20803d4f677f4e9
Artur Allmann <55272172+arturallmann@users.noreply.github.com>
2021-02-12 11:00:32 +0200

    Tund 4: lisasin kalkulaatori failid reposse. pean kalkulaatori veel tööle saama.

---
+++ Calculator/Calculator/CalculatorActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    [Activity(Label = "CalculatorActivity")]
+    public class CalculatorActivity : Activity
+    {
+
+        EditText firstEditText;
+        EditText secondEditText;
+        TextView answerText;
+        Button addButton;
+        Button subtractButton;
+        Button multiplyButton;
+        Button divideButton;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.calculator_layout);
+
+            firstEditText = FindViewById<EditText>(Resource.Id.firstNumberEditText);
+            secondEditText = FindViewById<EditText>(Resource.Id.secondNumberEditText);
+            var addButton = FindViewById<Button>(Resource.Id.addButton);
+            answerText = FindViewById<TextView>(Resource.Id.answerTextView);
+        }
+    }
+
+    private void AddButton_Click(object sender, EventArgs e)
+    {
+        var answer = int.Parse(firstEditText.Text) + int.Parse(secondEditText.Text);
+        answerText.Text = answer.ToString();
+    }
+    private void SubtractButton_Click(object sender, EventArgs e)
+    {
+        var answer = int.Parse(firstEditText.Text) - int.Parse(secondEditText.Text);
+        answerText.Text = answer.ToString();
+    }
+
+    private void MultiplyButton_Click(object sender, EventArgs e)
+    {
+        var answer = int.Parse(firstEditText.Text) * int.Parse(secondEditText.Text);
+        answerText.Text = answer.ToString();
+    }
+    
+        private void DivideButton_Click(object sender, EventArgs e)
+    {
+        var answer = int.Parse(firstEditText.Text) / int.Parse(secondEditText.Text);
+        answerText.Text = answer.ToString();
+    }
+
+}
