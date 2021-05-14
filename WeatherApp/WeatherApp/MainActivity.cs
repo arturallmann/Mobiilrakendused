6f3ff398a64a8cd6aa29a4b9ac2844908abe0884
Artur Allmann <55272172+arturallmann@users.noreply.github.com>
2021-05-14 15:11:45 +0300

    weatherapp

---
+++ WeatherApp/WeatherApp/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace WeatherApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var cityTextView = FindViewById<TextView>(Resource.Id.cityTextView);
+            var tempTextView = FindViewById<TextView>(Resource.Id.tempTextView);
+
+            var weatherService = new WeatherService();
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
