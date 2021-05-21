b7dbf52016b8ab1cc3c0ee25faa570a22b473030
Artur Allmann <55272172+arturallmann@users.noreply.github.com>
2021-05-21 14:42:15 +0300

    tegin muutused, kuid ei saanud andmeid õigesse formaati.

--- WeatherApp/WeatherApp/WeatherApp/MainActivity.cs
+++ WeatherApp/WeatherApp/WeatherApp/MainActivity.cs
@@ -1,8 +1,10 @@
 ﻿using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
+using System;
 using WeatherApp.Service;
 
 namespace WeatherApp
@@ -10,6 +12,7 @@ namespace WeatherApp
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        SearchView citySearch;
         protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -20,14 +23,28 @@ namespace WeatherApp
             var cityTextView = FindViewById<TextView>(Resource.Id.cityTextView);
             var tempTextView = FindViewById<TextView>(Resource.Id.tempTextView);
             var weatherImage = FindViewById<ImageView>(Resource.Id.weatherImage);
+            citySearch = FindViewById<SearchView>(Resource.Id.citySearchView);
+            
 
             var weatherService = new WeatherService();
+            citySearch.SearchClick += citySearch_Click;
+            var cityText = citySearch.ToString;
 
-            var weatherInfo = await weatherService.GetCityWeather("Tallinn");
+            var weatherInfo = await weatherService.GetCityWeather(cityText);
             cityTextView.Text = weatherInfo.Name;
             tempTextView.Text = weatherInfo.Main.Temp.ToString();
 
             weatherImage.SetImageBitmap(await weatherService.GetImageFromUrl($"https://openweathermap.org/img/wn/{weatherInfo.Weather[0].Icon}@2x.png"));
+        }
+        public void citySearch_Click(object sender, EventArgs e)
+        {
+            
+            var citySearchView = FindViewById<SearchView>(Resource.Id.citySearchView);
+            string searchText = citySearchView.ToString();
+            
+            citySearch = searchText;
+            
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
