c9e44c547b465ea166ea33fc3f10f4d352f25dcc
Artur Allmann <55272172+arturallmann@users.noreply.github.com>
2021-05-28 13:02:57 +0300

    tegin films asjad valmis, tegelen planetsiga

---
+++ StarwarsApp/StarwarsApp/FilmsDetailActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using StarwarsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarwarsApp
+{
+    [Activity(Label = "FilmsDetailActivity")]
+    public class FilmsDetailActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.films_detail_layout);
+
+            var titleText = FindViewById<TextView>(Resource.Id.titleTextView);
+            var episodeText = FindViewById<TextView>(Resource.Id.episodeTextview);
+            var descriptionText = FindViewById<TextView>(Resource.Id.descriptionTextview);
+            var directorText = FindViewById<TextView>(Resource.Id.directorTextview);
+            var producerText = FindViewById<TextView>(Resource.Id.producerTextview);
+            var releaseText = FindViewById<TextView>(Resource.Id.releaseDateTextview);
+
+            var FilmsDetailJson = Intent.GetStringExtra("PeopleDetail");
+            var details = JsonConvert.DeserializeObject<FilmsDetails>(FilmsDetailJson);
+
+            titleText.Text += details.title;
+            episodeText.Text += details.episode_id;
+            descriptionText.Text += details.opening_crawl;
+            directorText.Text += details.director;
+            releaseText.Text += details.release_date;
+            // Create your application here
+
+        }
+    }
+}
---
+++ StarwarsApp/StarwarsApp/PlanetsActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using StarwarsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarwarsApp
+{
+    [Activity(Label = "PlanetsActivity")]
+    public class PlanetsActivity : Activity
+    {
+        Planets planetsdata = new Planets();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
